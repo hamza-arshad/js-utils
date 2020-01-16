62ed43f59a046a18c50f4d457352f23e45e13991
inker <anton.veselyev@gmail.com>
2020-01-16 10:40:55 +0000

    update ordinal

--- src/ordinal.ts
+++ src/ordinal.ts
@@ -3,23 +3,27 @@ import isNaturalNumber from './isNaturalNumber'
 const got = (i: any) =>
   typeof i === 'string' ? JSON.stringify(i) : i
 
+function getOrdinalSuffix(i: number) {
+  const j = i % 10
+  const k = (i % 100) - j
+  if (k !== 10) {
+    if (j === 1) {
+      return 'st'
+    }
+    if (j === 2) {
+      return 'nd'
+    }
+    if (j === 3) {
+      return 'rd'
+    }
+  }
+  return 'th'
+}
+
 export default (i: number) => {
   if (!isNaturalNumber(i)) {
     throw new Error(`Number must be a natural number. Instead got: ${got(i)}`)
   }
 
-  const j = i % 10
-  const k = (i % 100) - j
-  if (k !== 10) {
-    if (j === 1) {
-      return `${i}st`
-    }
-    if (j === 2) {
-      return `${i}nd`
-    }
-    if (j === 3) {
-      return `${i}rd`
-    }
-  }
-  return `${i}th`
+  return `${i}${getOrdinalSuffix(i)}`
 }
